721fb9977f73631132c1e7c299a3f66d159d270c
Welington Fidelis <welingtonfidelis@gmail.com>
2024-04-25 08:35:00 -0300

    fullCV html started;

--- src/pages/curriculum/index.tsx
+++ src/pages/curriculum/index.tsx
@@ -1,21 +1,146 @@
 import { PDFViewer } from "@react-pdf/renderer";
 import { useTranslation } from "react-i18next";
 import {
+  AboutDescription,
+  ContactContent,
+  Container,
+  EducationContainer,
+  EducationCourseText,
+  EducationDateText,
+  EducationInstitutionText,
+  EducationItemContent,
   FaEnvelopeIcon,
   FaGithubIcon,
   FaGlobeAmericasIcon,
   FaLinkedinIcon,
   FaMapMarkerIcon,
+  HorizontalSeparator,
+  LanguageContainer,
+  LanguageText,
+  ProfileAbout,
+  ProfileImage,
+  ProfileName,
+  SectionLeft,
+  SectionLeftTitle,
+  SectionRight,
+  SectionRightTitle,
+  ServiceContent,
+  ServiceSectionLeftContent,
+  ServiceSectionLeftText,
+  ServiceSectionRightContent,
+  ServiceSectionRightTitle,
 } from "./styles";
 import { FullCurriculumPdf } from "../../components/pdf/fullCurriculum";
+import { LightCurriculumPdf } from "../../components/pdf/lightCurriculum";
+import { EducationInterface } from "../../domains/Education";
+import { JobInterface } from "../../domains/Job";
 
 export const Curriculum = () => {
   const { t } = useTranslation();
 
   return (
-    <div>
-      {/* TODO remove PDFViewer that when create Download mode */}
-      <FullCurriculumPdf />
-    </div>
+    /* TODO remove PDFViewer that when create Download mode */
+
+    // <FullCurriculumPdf />
+
+    // <LightCurriculumPdf />
+
+    <Container>
+      <SectionLeft>
+        <ProfileImage>
+          <img src={"/public/images/user_1.jpg"} alt="" />
+        </ProfileImage>
+
+        <ProfileName>{t("about_me.name")}</ProfileName>
+
+        <ProfileAbout>{t("about_me.office")}</ProfileAbout>
+
+         <HorizontalSeparator />
+
+        <SectionLeftTitle>{t("contact.subtitle")}</SectionLeftTitle>
+        <ContactContent>
+          <span>{t("phone")}</span>
+          <span>{t("email")}</span>
+          <span>{t("site")}</span>
+          <span>{t("linkedin")}</span>
+          <span>{t("github")}</span>
+          <span>{t("location")}</span>
+        </ContactContent>
+
+        <SectionLeftTitle>{t("education.title")}</SectionLeftTitle>
+        <EducationContainer>
+          {(
+            t("education.list", {
+              returnObjects: true,
+            }) as EducationInterface[]
+          ).map((item, index) => (
+            <EducationItemContent key={index}>
+              <EducationDateText>
+                {item.start} - {item.end}
+              </EducationDateText>
+
+              <EducationCourseText>{item.title}</EducationCourseText>
+
+              <EducationInstitutionText>
+                {item.institution}
+              </EducationInstitutionText>
+            </EducationItemContent>
+          ))}
+        </EducationContainer>
+
+        <SectionLeftTitle>{t("language.title")}</SectionLeftTitle>
+
+        <LanguageContainer>
+          <LanguageText>
+            {t("language.portuguese")} - {t("language.portuguese_level")}
+          </LanguageText>
+
+          <LanguageText>
+            {t("language.english")} - {t("language.english_level")}
+          </LanguageText>
+        </LanguageContainer>
+      </SectionLeft>
+
+      <SectionRight>
+        <SectionRightTitle>{t("about_me.title")}</SectionRightTitle>
+        <AboutDescription>{t("about_me.description_part_1")}</AboutDescription>
+        <AboutDescription>{t("about_me.description_part_2")}</AboutDescription>
+        <AboutDescription>{t("about_me.description_part_3")}</AboutDescription>
+
+        <SectionRightTitle>{t("services.title")}</SectionRightTitle>
+        {(
+          t("services.list", {
+            returnObjects: true,
+          }) as JobInterface[]
+        ).map((item, index) =>  (
+          <ServiceContent key={index}>
+            <ServiceSectionLeftContent>
+              <ServiceSectionLeftText>
+                {item.start} - {item.end}
+              </ServiceSectionLeftText>
+              <ServiceSectionLeftText>
+                {item.company_name}
+              </ServiceSectionLeftText>
+            </ServiceSectionLeftContent>
+
+            <ServiceSectionRightContent>
+              <ServiceSectionRightTitle>
+                {item.position}
+              </ServiceSectionRightTitle>
+              <span>{item.description}</span>
+
+              {item.technologies && (
+                <>
+                  <br />
+
+                  <span>{t("technologies_contact")}:</span>
+                  <span>{item.technologies}</span>
+                </>
+              )}
+            </ServiceSectionRightContent>
+          </ServiceContent>
+        ))}
+      </SectionRight>
+    </Container>
   );
 };
--- src/pages/curriculum/styles.ts
+++ src/pages/curriculum/styles.ts
@@ -7,6 +7,14 @@ import {
 } from "react-icons/fa";
 import styled from "styled-components";
 
+const colorTheme = {
+  background_a: "#003147",
+  background_b: "#fff",
+  primary: "#333",
+  secondary: "#0388c5",
+  tertiary: "#6c6d6e",
+};
+
 export const FaEnvelopeIcon = styled(FaEnvelope)``;
 
 export const FaGlobeAmericasIcon = styled(FaGlobeAmericas)``;
@@ -16,3 +24,156 @@ export const FaLinkedinIcon = styled(FaLinkedin)``;
 export const FaGithubIcon = styled(FaGithub)``;
 
 export const FaMapMarkerIcon = styled(FaMapMarker)``;
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  background-color: ${colorTheme.background_a};
+  height: 100%;
+  /* letter-spacing: 0.5px; */
+  /* line-height: 1.1px; */
+  /* font-size: 10.5px; */
+`;
+
+export const HorizontalSeparator = styled.div`
+  border-bottom: 1px solid hsla(0, 0%, 100%, 0.2);
+  width: 100%;
+`;
+
+export const SectionLeft = styled.div`
+  padding: 30px;
+  color: ${colorTheme.background_b};
+  display: flex;
+  flex-direction: column;
+  width: 19rem;
+`;
+
+export const ProfileImage = styled.div`
+  img {
+    margin: auto;
+    border-radius: 50%;
+    width: 13rem;
+    height: 13rem;
+    object-fit: cover;
+  }
+`;
+
+export const ProfileName = styled.h2`
+  margin-top: 1.5rem;
+  text-transform: uppercase;
+  text-align: center;
+  font-weight: 600;
+  font-size: 20px;
+`;
+
+export const ProfileAbout = styled.span`
+  margin: 0.5rem 0 2rem 0;
+  text-transform: uppercase;
+  text-align: center;
+`;
+
+export const SectionLeftTitle = styled.span`
+  margin-top: 2rem;
+  margin-bottom: 11px;
+  text-transform: uppercase;
+  font-weight: 500;
+  color: ${colorTheme.secondary};
+`;
+
+export const ContactContent = styled.div`
+  margin-left: 0.5rem;
+  font-size: 14px;
+  display: flex;
+  flex-direction: column;
+  line-height: 26px;
+`;
+
+export const EducationContainer = styled.div`
+  margin-left: 0.5rem;
+  font-size: 14px;
+  display: flex;
+  flex-direction: column;
+  line-height: 26px;
+`;
+
+export const EducationItemContent = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const EducationDateText = styled.span`
+  margin-bottom: 6px;
+`;
+
+export const EducationCourseText = styled.span`
+  margin-bottom: 6px;
+  font-weight: 600;
+`;
+
+export const EducationInstitutionText = styled.span`
+  margin-bottom: 6px;
+`;
+
+export const LanguageContainer = styled.div`
+  margin-left: 0.5rem;
+  font-size: 14px;
+  display: flex;
+  flex-direction: column;
+  line-height: 26px;
+`;
+export const LanguageText = styled.span`
+  margin-bottom: 6px;
+`;
+
+export const SectionRight = styled.div`
+  display: flex;
+  flex-direction: column;
+  background-color: ${colorTheme.background_b};
+  color: ${colorTheme.primary};
+  flex: 1;
+  padding: 0 2rem;
+`;
+
+export const SectionRightTitle = styled.span`
+  font-size: 20px;
+  font-weight: 500;
+  margin-top: 2rem;
+  margin-bottom: 1rem;
+  text-transform: uppercase;
+  color: ${colorTheme.background_a};
+`;
+
+export const AboutDescription = styled.span`
+  margin-bottom: 12px;
+`;
+
+export const ServiceContent = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin-bottom: 1.5rem;
+`;
+
+export const ServiceSectionLeftContent = styled.div`
+  width: 100px;
+  display: flex;
+  flex-direction: column;
+  width: 8.5rem;
+`;
+
+export const ServiceSectionLeftText = styled.span`
+  color: ${colorTheme.tertiary};
+  margin-bottom: 6px;
+  text-transform: uppercase;
+`;
+
+export const ServiceSectionRightContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+`;
+
+export const ServiceSectionRightTitle = styled.span`
+  text-transform: uppercase;
+  color: ${colorTheme.secondary};
+  margin-bottom: 10px;
+`;
